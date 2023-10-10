cbbebe71a7f1f6a41a1cad6c34abffb6facd4f92
Krishan Chakroborty <138010864+krisHChakroborty@users.noreply.github.com>
2023-10-10 17:36:27 +0530

    new update

---
+++ jequery.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+
+
+
+
+    $(".navbar-toggler").click(function() {
+
+
+        $(".social2").toggle(); // This will hide the navbar with the id "navbar"
+
+
+    });
+
+
+
+});
